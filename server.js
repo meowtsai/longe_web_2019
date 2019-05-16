c7f1e80ebcb71bc10e805325c117c07da5fbdfa8
meowtsai <shihfan.tsai@gmail.com>
2019-05-16 16:43:55 +0800

    change port

--- server.js
+++ server.js
@@ -30,6 +30,14 @@ app.use("/api/service", service);
 app.use("/api/questions", questions);
 app.use("/api/upload", upload);
 
+//serve static build (client)
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const port = CONFIG.port || 5000;
 
 app.listen(port, () => {
