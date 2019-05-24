0a96764cbe794a0b429c761270fe05ebe19a80f9
meowtsai <shihfan.tsai@gmail.com>
2019-05-24 12:09:52 +0800

    add fetch

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "core-js";
 import "regenerator-runtime/runtime";
+import "whatwg-fetch";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
