e361ff899fb4dd1c2b94319587f0713568d28427
meowtsai <shihfan.tsai@gmail.com>
2021-06-20 15:44:40 +0800

    allow origin

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
-const express = require("express");
-const path = require("path");
-var logger = require("morgan");
-var rfs = require("rotating-file-stream");
-const fileUpload = require("express-fileupload");
-const games = require("./routes/api/games");
-const service = require("./routes/api/service");
-const questions = require("./routes/api/questions");
-const upload = require("./routes/api/upload");
-const events = require("./routes/api/events");
-const vip = require("./routes/api/vip");
-const CONFIG = require("./config/config")[process.env.NODE_ENV];
-const requestIp = require("request-ip");
-const helmet = require("helmet");
+const express = require('express');
+const path = require('path');
+var logger = require('morgan');
+var rfs = require('rotating-file-stream');
+const fileUpload = require('express-fileupload');
+const games = require('./routes/api/games');
+const service = require('./routes/api/service');
+const questions = require('./routes/api/questions');
+const upload = require('./routes/api/upload');
+const events = require('./routes/api/events');
+const vip = require('./routes/api/vip');
+const fbapi = require('./routes/api/fbapi');
+const CONFIG = require('./config/config')[process.env.NODE_ENV];
+const requestIp = require('request-ip');
+const helmet = require('helmet');
 // default options
 // create a rotating write stream
-var accessLogStream = rfs("access.log", {
-  interval: "1d", // rotate daily
-  path: path.join(__dirname, "log")
+var accessLogStream = rfs('access.log', {
+  interval: '1d', // rotate daily
+  path: path.join(__dirname, 'log'),
 });
 const app = express();
-app.set("trust proxy", true);
-app.use(logger("combined", { stream: accessLogStream }));
+app.set('trust proxy', true);
+app.use(logger('combined', { stream: accessLogStream }));
 app.use(
   fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 },
     abortOnLimit: true,
-    responseOnLimit: "檔案太大",
-    limitHandler: function(req, res, next) {
+    responseOnLimit: '檔案太大',
+    limitHandler: function (req, res, next) {
       //console.log("hi");
-      return res.status(413).send({ file01: "檔案太大" });
-    }
+      return res.status(413).send({ file01: '檔案太大' });
+    },
   })
 );
 
@@ -38,10 +39,30 @@ app.use(requestIp.mw());
 
 app.use(helmet());
 
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.identity-v.com/');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 //check if ip is in whitelist
-app.use(function(req, res, next) {
-  const white_list = require("./config/config").white_list;
-  const found = white_list.find(item => item.ip === req.clientIp);
+app.use(function (req, res, next) {
+  const white_list = require('./config/config').white_list;
+  const found = white_list.find((item) => item.ip === req.clientIp);
   //console.log("req.clientIp", req.clientIp);
   if (found) {
     req.whitelisted = true;
@@ -52,38 +73,39 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use("/api/games", games);
-app.use("/api/service", service);
-app.use("/api/events", events);
-app.use("/api/questions", questions);
-app.use("/api/upload", upload);
-app.use("/api/vip", vip);
+app.use('/api/games', games);
+app.use('/api/service', service);
+app.use('/api/events', events);
+app.use('/api/questions', questions);
+app.use('/api/upload', upload);
+app.use('/api/vip', vip);
+app.use('/api/fbapi', fbapi);
 
-app.use("/uploads", express.static("client/public/uploads"));
+app.use('/uploads', express.static('client/public/uploads'));
 
 //serve static build (client)
-if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "stage") {
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'stage') {
   //set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
 const port = CONFIG.port || 5000;
 
 let server;
-if (app.get("env") !== "production") {
-  var http = require("http");
+if (app.get('env') !== 'production') {
+  var http = require('http');
 
   server = http.createServer(app);
 } else {
-  var fs = require("fs");
-  const https = require("https");
+  var fs = require('fs');
+  const https = require('https');
   var options = {
     key: fs.readFileSync(CONFIG.ssl_options.keyfile),
     cert: fs.readFileSync(CONFIG.ssl_options.certfile),
-    ca: [fs.readFileSync(CONFIG.ssl_options.cafile)]
+    ca: [fs.readFileSync(CONFIG.ssl_options.cafile)],
   };
 
   server = https.createServer(options, app);
